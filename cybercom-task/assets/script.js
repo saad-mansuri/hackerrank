502eb29766380464372b32d0be2df65add2f1534
saad-mansuri <saadmansuri249@gmail.com>
2023-05-14 14:05:40 +0530

    just commit

--- cybercom-task/assets/script.js
+++ cybercom-task/assets/script.js
@@ -1,41 +1,13 @@
 $(document).ready(function(){
+    var productData = "productData";
     let allProducts = []
     let initProd = []
     init()
     function init(){
         let getallProducts = JSON.parse(localStorage.getItem('cyberC'))
-        initProd.push(getallProducts)
         console.log(getallProducts);
     }
-    console.log(initProd);
-    console.log(allProducts);
     $('#btnSave').on('click', AddProduct)
-
-    // function formValidation(e){
-    //     e.preventDefault()
-    //     $('#prodForm').validate({
-    //         rules : {
-    //             prodTitle : "required",
-    //             prodPrice : "required",
-    //             productDescription : "required",
-    //             productCategory : "required",
-    //             prodImage : "required"
-    //         },
-    //         message: {
-    //             prodTitle: "Please Enter Your First Name",
-    //             prodPrice: "Please Enter Your Last Name",
-    //             productDescription: "Please Enter Your Email",
-    //             productCategory: "Please Enter Your Company",
-    //             prodImage: "Please Enter Your Company",
-    //         }
-    //     })
-
-
-    //     if($('#prodForm').valid()){
-    //         AddProduct()
-    //     }
-    // }
-
     function AddProduct(e){
         e.preventDefault()
 
@@ -56,6 +28,13 @@ $(document).ready(function(){
         }
 
         allProducts.push(setAllProducts)
-        localStorage.setItem('cyberC', JSON.stringify(allProducts))
+        getProductDataObj(JSON.stringify(allProducts))
+        console.log(initProd);
+        console.log(allProducts);
     }
 })
+
+function getProductDataObj(getData){
+    console.log(getData);
+    // localStorage.setItem('cyberC', getData)
+}
